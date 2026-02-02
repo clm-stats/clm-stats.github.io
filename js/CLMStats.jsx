a6700aa91ecd19e9b121523c5d2c2e9884106e48
Mitch Dzugan <mitchdzugan@gmail.com>
2026-02-02 14:49:34 -0600

    begin adding db files

--- js/CLMStats.jsx
+++ js/CLMStats.jsx
@@ -1,5 +1,5 @@
 export default function CLMStats() {
-  fetch('/db/players.json').then(res => res.json()).then(console.log).catch(console.log)
+  fetch('/db/periods/9.json').then(res => res.json()).then(console.log).catch(console.log)
   return (
     <div>
       CLM Stats Page
