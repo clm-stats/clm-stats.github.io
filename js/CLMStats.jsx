68390c862010d9cd961df8cf87911c433eefa0a8
Mitch Dzugan <mitchdzugan@gmail.com>
2026-02-02 13:43:33 -0600

    repo setup

--- js/CLMStats.jsx
+++ js/CLMStats.jsx
@@ -1,5 +1,10 @@
 export default function CLMStats() {
+  fetch('/db/players.json').then(res => res.json()).then(console.log).catch(console.log)
   return (
-    <div>CLM Stats Page</div>
+    <div>
+      CLM Stats Page
+      <pre>my code</pre>
+      xDD
+    </div>
   );
 }
