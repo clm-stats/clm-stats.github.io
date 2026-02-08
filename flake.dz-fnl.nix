a1ae111cc7f3c6cccf929aff492da69fff5a64fc
Mitch Dzugan <mitchdzugan@gmail.com>
2026-02-07 21:50:44 -0600

    begin fennel setup

---
+++ flake.dz-fnl.nix
@@ -0,0 +1,8 @@
+{
+  name = "midlane-cv";
+  version = "0.0.1-0";
+  mkLuaDeps = env: [
+    (env.lua-__.mkPkg env.pkgs)
+    env.luaPackages.rapidjson
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "fennel util library";
+  inputs = {
+    lua-__.url = "path:/VOID/proj/__.lua";
+    mitch-utils.url = "path:/VOID/proj/mitch-utils.nix";
+    # mitch-utils.url = "github:mitchdzugan/mitch-utils.nix";
+  };
+  outputs = inputs@{ mitch-utils, ... }: (mitch-utils.mkZnFnl inputs ./.);
+}
